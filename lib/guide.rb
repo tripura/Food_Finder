a50ece387d8909585bd3f8e8b11a76ebb42584cf
Tripura Konda <tripura@eggheadcreative.com>
2013-05-06 15:36:39 +0530

    method added to perform actions-list , find , add , quit

--- lib/guide.rb
+++ lib/guide.rb
@@ -15,10 +15,29 @@ class Guide
 
   def launch!
     introduction
-
+    loop do
+      print "> "
+      user_response = gets.chomp
+      result = do_action(user_response)
+      break if result == :quit
+    end
     conclusion
   end
 
+  def do_action(action)
+    case action
+    when 'list'
+      puts "Listing...."
+    when 'find'
+      puts "Finding...."
+    when 'add'
+      puts "Adding...."
+    when 'quit'
+      return :quit
+    else
+      puts "\n I don't understand the command./n"
+    end
+  end
   def introduction
     puts "\n\n<<<Welcome to Food Finder>>>\n\n"
     puts "This is an interactive guide that helps you to find the food that you crave.\n"
