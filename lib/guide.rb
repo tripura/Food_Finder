d7d1e8b4db2c50cfde01b3af6c35226554a0185d
Tripura Konda <tripura@eggheadcreative.com>
2013-05-06 12:31:24 +0530

    Adding library to Food_Finder Project.

---
+++ lib/guide.rb
@@ -0,0 +1,30 @@
+class Guide
+
+  def initialize(path=nil)
+    Restaurant.filepath = path
+    if Restaurant.file_exists?
+       puts "Found Restaurant file."
+    elsif Restaurant.create_file
+       puts "Created Restaurant file."
+    else 
+       puts "Exiting./n/n"
+       exit!
+    end
+  end
+
+  def launch!
+    introduction
+
+    conclusion
+  end
+
+  def introduction
+    puts "\n\n<<<Welcome to Food Finder>>>\n\n"
+    puts "This is an interactive guide that helps you to find the food that you crave.\n"
+  end
+
+  def conclusion
+    puts "\n\n<< GoodBye and Bon Apetit .>>\n\n"
+  end
+end
+ 
---
+++ lib/restaurant.rb
@@ -0,0 +1,17 @@
+class Restaurant
+  @@filepath = nil
+  def self.filepath = (path=nil)
+    @@filepath = File.join(APP_ROOT, path)
+  end
+  def self.file_exists?
+
+  end
+
+  def self.create_file
+
+  end
+
+  def self.saved_restaurants
+
+  end
+end 
